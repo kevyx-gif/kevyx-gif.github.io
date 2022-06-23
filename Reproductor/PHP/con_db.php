69d7e244c7eb67e552971b007877c1536214b306
kevy-gif <apolortms@gmail.com>
2022-06-23 16:07:50 -0500

    cambios

--- Reproductor/PHP/con_db.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$conex = mysqli_connect("localhost","root","Cremita*123","reproductor");
-
-?>
--- Reproductor/PHP/paso3.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include("con_db.php");
-    session_start();
-    unset($_GET['var']);
-    header('Location: http://localhost:3000/PHP/Perfiles.php');
-    exit;
-?>
